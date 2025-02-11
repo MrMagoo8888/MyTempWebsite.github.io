08400ec8d6444ce29102d072250268a9625be5f5
MrMagoo8888 <amwilson218@gmail.com>
2025-02-11 20:23:55 +0000

    Update Bobby.js

--- Bobby.js
+++ Bobby.js
@@ -4,5 +4,5 @@ const gifElement = document.querySelector('.gif');
 
 revealButton.addEventListener('click', () => {
   storyElement.textContent = "No one ever saw Bobby again, but sometimes, on stormy nights, the neighbors would hear the faint sound of a child’s laughter carried on the wind, and they would see a small figure standing in the attic window, staring out with dark, empty eyes. And those who looked too closely into the shadows would swear they could hear a voice whispering, “I’m coming for you next... I'm hungry for your soul.”";
-  gifElement.src = "/Bobbygifs/jumpscare-cat.gif"; 
+  gifElement.src = "https://mrmagoo8888.github.io/MyTempWebsite.github.io/Bobbygifs/jumpscare-cat.gif"; 
 })
